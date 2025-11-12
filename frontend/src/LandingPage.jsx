49fef7baf3459e9077c37dd66647daaf123486fc
kristopher L Wiley <kriswiley0421@gmail.com>
2025-11-11 18:17:23 -0700

    fixed an unused statement

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import DragMapArea from "./DragMapArea";
 import MapEditor from "./MapEditor";
-import SignIn from "./SignIn";
+
 
 
 
